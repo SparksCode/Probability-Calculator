67948db710181029933ded5b53877cf088eae14c
Zachary Sparks <>
2021-04-08 17:47:28 +0000

    Build out Hat class.

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,17 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+  def __init__(self, **kargs):
+    self.contents = []
+    for key, value in kargs.items():
+      for i in range(0, value, 1):
+        self.contents.append(key)
 
+  def draw(self, draws):
+    drawn = []
+    for i in range(0, draws, 1):
+      drawn.append(self.contents.pop(random.randint(0, len(self.contents)-1)))
+    return drawn
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  return ""
